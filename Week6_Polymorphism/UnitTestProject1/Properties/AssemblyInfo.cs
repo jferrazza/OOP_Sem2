a57eefe33cb0f4c878222177a150b2af35617c02
jferrazza <102116925@student.swin.edu.au>
2018-09-17 11:04:24 +1000

    17/09

---
+++ Week6_Polymorphism/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21b24ca3-fa08-4d42-8338-f9b363bdf0bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week6_Polymorphism/Week6_Polymorphism/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_Polymorphism
+{
+    abstract public class Weapon
+    {
+
+        public int BaseDamage { get; protected set; }
+        public int BaseRange { get; protected set; }
+        public int CriticalDamage { get; protected set; }
+        public int ActionPoints { get; protected set; }
+
+        virtual public int Hit() { throw new NotImplementedException(); }
+
+        public Weapon(int baseDamage, int baseRange, int criticalDamage)
+        {
+            BaseDamage = baseDamage;
+            BaseRange = baseRange;
+            CriticalDamage = criticalDamage;
+            ActionPoints = 0;
+        }
+    }
+
+
+    public class Malee : Weapon
+    {
+        public Malee(int baseDamage, int baseRange, int criticalDamage) : base(baseDamage, baseRange, criticalDamage) { }
+
+        public override int Hit()
+        {
+            var r = new Random();
+            ActionPoints++;
+            return r.Next(100) < 10 ? CriticalDamage : BaseDamage;
+        }
+
+    }
+    public class Range : Weapon
+    {
+        public Range(int baseDamage, int baseRange, int criticalDamage) : base(baseDamage, baseRange, criticalDamage) { }
+
+        public override int Hit()
+        {
+            var r = new Random();
+            var rr = r.Next(100) < 5;
+            if (rr) ActionPoints++;
+            return rr ? CriticalDamage : BaseDamage;
+        }
+
+    }
+}
